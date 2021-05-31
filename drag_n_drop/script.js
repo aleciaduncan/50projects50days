378cc399a0d81fd4d9e61e97f11f7d9e77b31210
Alecia Duncan <aleciamduncan@gmail.com>
2021-05-31 13:35:38 -0600

    Image can be drag and dropped into other boxes, minor formatting update

--- drag_n_drop/script.js
+++ drag_n_drop/script.js
@@ -1,8 +1,8 @@
 const fill = document.querySelector('.fill');
 const empties = document.querySelectorAll('.empty');
 
-fill.addEventListener('dragstart', dragStart());
-fill.addEventListener('dragend', dragEnd());
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
 
 for(const empty of empties){
     empty.addEventListener('dragover', dragOver);
