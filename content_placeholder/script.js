8a1a07b63e14a48929ac58cf7e3fd02389168b6a
Alecia Duncan <aleciamduncan@gmail.com>
2021-05-31 14:42:16 -0600

    Scrolling linear gradient background serves as content placeholder

---
+++ content_placeholder/script.js
@@ -0,0 +1,25 @@
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profile_img = document.getElementById('profile_img');
+const authName = document.getElementById('name');
+const date = document.getElementById('date');
+
+const animated_bgs = document.querySelectorAll('.animated-bg');
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
+
+setTimeout(getData, 2500);
+
+function getData(){
+    header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhvvHBfaWQiOjEyMDd9&auto=format&fit=crop&w=210&q=80" alt="">';
+    title.innerHTML = 'Lorem ipsum dolor sit amet.';
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, cupiditate.';
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/women/12.jpg" alt="">';
+    authName.innerHTML = 'Jane Doe';
+    date.innerHTML = 'May 21, 2021';
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'));
+    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'));
+}
+
+
